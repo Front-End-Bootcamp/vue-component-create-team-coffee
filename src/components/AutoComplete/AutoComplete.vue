9f34dcb5cbd6e1afa2b6ac5f07e2acc965e2eef0
trombonist76 <mahmut.hg@gmail.com>
2022-10-12 21:13:33 +0300

    [Update] Autocomplete renewed.

--- src/components/AutoComplete/AutoComplete.vue
+++ src/components/AutoComplete/AutoComplete.vue
@@ -1,136 +1,111 @@
 <script setup>
-import { computed, ref } from "vue";
-const props = defineProps(["data", "filterBy", "title"]);
-const emit = defineEmits(['setSelected']);
-const input = ref(null);
-const optionsList = ref(null);
-const itemHeight = ref(39);
-const selectedItem = ref(null);
-const visible = ref(true);
-const query = ref('');
+import { ref, computed } from 'vue';
+import { useClickOnTarget } from '@/composables/clickOnTarget';
+const props = defineProps(["data"])
+const emits = defineEmits(["setSelected"])
+const wrapper = ref(null)
+const showResults = ref(true)
+const search = ref("")
+const skipUnwrap = { el: ref([]) };
+const selectedOption = ref(null)
+const isClickOnTarget = useClickOnTarget(wrapper)
+const filterBySearch = computed(() => props.data.filter(option => option.name.toLowerCase().includes(search.value.toLowerCase())))
 
-const matches = computed(() => {
-	return props.data.filter((item) => item[props.filterBy].toLowerCase().includes(query.value.toLowerCase()))
-})
-
-const itemClicked = (match) => {
-	selectedItem.value = match;
-	query.value = match.name;
-	emit('setSelected', selectedItem.value);
+const selectOptionHandler = (option) => {
+  search.value = ""
+  selectedOption.value = option
+  showResults.value = false
+  emits("setSelected", option)
 }
 
-const remove = () => {
-	query.value = '';
-	selectedItem.value = 0;
+const typeHandler = (event) => {
+  search.value = event.target.value
+  showResults.value = true
+  selectedOption.value = null
 }
 
-// const scrollToItem = () => {
-// 	return optionsList.value.scrollTop = selected.value * itemHeight.value;
-// }
+const clearSearchHandler = () => {
+  search.value = ""
+  selectedOption.value = null
+}
 
+
+const clickInputHandler = () => {
+  showResults.value = true
+}
 </script>
+
+
 <template>
-	<!-- <div class="autocomplete"> -->
-	<div class="container">
-		<pre>{{matches}}</pre>
-		<div class="popover" v-show="visible">
-			<div class="input-wrapper">
-				<input class="popover__input" type="text" ref="input" v-model.trim="query"
-					@keydown.enter="selectItem" placeholder="Start Typing...">
-				<button class="close" @click="remove " v-if="selectedItem">X</button>
-			</div>
+  <div ref="wrapper" class="autocomplete">
+    <div @click="clickInputHandler" class="autocomplete__input">
+      <input  class="search" placeholder="Type for search" type="text" :value="search || selectedOption?.name" @input.trim="typeHandler"/>
+      <span  @click="clearSearchHandler" class="material-icons close">close</span>
+      <span :class="{'active': isClickOnTarget & showResults}" class="material-icons expand">expand_more</span>
 
-			<div class="options" ref="optionsList">
-				<ul>
-					<li v-for="match in matches" :key="match.id" :class="{ 'selected': (selectedItem?.id == match.id)}"
-						@click="itemClicked(match)" v-text="match[props.filterBy]"></li>
-				</ul>
-			</div>
-		</div>
-	</div>
+    </div>
 
+    <div v-if="filterBySearch.length > 0" v-show="isClickOnTarget && showResults" class="autocomplete__results">
 
-	<!-- </div> -->
+      <button 
+        :ref="skipUnwrap.el" 
+        @click="selectOptionHandler(option)" 
+        class="result" 
+        :class="{'selected' : selectedOption?.name === option.name}" 
+        v-for="option in filterBySearch">
+        {{option.name}}
+      </button>
+
+    </div>
+    <div class="not-found" v-else>Your search - {{search}} - did not match any options.</div>
+  </div>
 </template>
-<style scoped>
-
-.container{
-	display: flex;
-}
-.popover {
-	min-width: 300px;
-	border: 2px solid lightgray;
-	border-radius: 25px;
-	overflow: hidden;
-	text-align: center;
-	justify-content: center;
-}
 
 
-.input-wrapper {
-	display: flex;
-}
+<style lang="scss" scoped>
+.autocomplete {
+  @apply w-[500px] my-5 mx-auto select-none;
 
-.popover__input {
-	flex: 1;
-	padding: 15px 20px;
-	font-size: 16px;
-	border-radius: 25px;
-}
+  &__input {
+    @apply flex gap-2 items-center h-12 rounded-md px-4 border border-slate-200;
 
-.close {
-	color: black;
-	padding: 0 8px;
-	cursor: pointer;
-}
+    .search{
+      @apply bg-inherit flex-1 outline-none
+    }
 
-.options {
-	max-height: 450px;
-	color: black;
-	align-items: center;
-	justify-content: center;
-	overflow-y: scroll;
+    .close{
+      @apply text-lg flex items-center p-1 px-2 rounded-full cursor-pointer
+    }
+
+    .expand{ 
+      @apply delay-75 transition-all flex items-center p-1 rounded-full cursor-pointer
+    }
+    .active{
+      @apply rotate-180
+    }
+
+  }
+
+  &__results {
+    @apply max-h-[300px] overflow-y-auto border rounded-md border-slate-200;
+
+    .result {
+      @apply flex py-2.5 px-4 border-b border-slate-200 w-full outline-none;
+
+      &:focus{
+        @apply bg-slate-700;
+      }
+
+    }
+
+    .selected{
+        @apply bg-slate-700;
+      }
+  }
 
 
-
-}
-
-.options ul {
-	/* list-style-type: none; */
-	text-align: left;
-	border-top-right-radius: 25px;
-	border-bottom-right-radius: 25px;
-	border-top-left-radius: 25px;
-	border-bottom-left-radius: 25px;
-
-}
-
-.options ul li {
-	list-style-type: none;
-	border-bottom: 1px solid lightgray;
-	padding: 10px;
-	cursor: pointer;
-	min-width: 100%;
-	background: #f1f1f1;
-	position: flex;
-	/* border-top-right-radius: 25px;
-		border-bottom-right-radius: 25px;
-		border-top-left-radius: 25px;
-		border-bottom-left-radius: 25px; */
-}
-
-.options ul li:first-child {
-	border-top: 2px solid #d6d6d6;
-}
-
-.options ul li:not(.selected):hover {
-	background: #bfb3b3;
-	color: #fff;
-}
-
-.options ul li.selected {
-	background: rgb(143, 135, 135);
-	color: #fff;
-	font-weight: 600;
+  .not-found{
+    @apply px-3 py-2 border border-slate-200 rounded-md break-words
+  }
 }
 </style>
